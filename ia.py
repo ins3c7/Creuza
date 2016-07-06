8762898f4371acf8b9b82ae37a7cf2a8e7878320
ins3c7 <ins3c7@hotmail.com>
2016-07-06 13:58:41 -0300

    upload files

--- ia.py
+++ ia.py
@@ -4,9 +4,9 @@
 # by ins3c7
 # 5 july 2016
 
-# I.A. 02
+# I.A. 02 [Creuza]
 
-import json, os, re
+import json, os, re, time
 from requests import get
 from random import choice
 from unidecode import unidecode as uni
@@ -16,34 +16,48 @@ os.system('clear')
 class iabot:
 	def __init__(self):
 		self.questioned = []
-		pass
+		self.filedb = 'db.ai'
+
+	def add(self):
+		print '\n+ put: question /// answer1 # answer2 (...)'
+		learn = raw_input('+ ')
+		with open(self.filedb, 'a') as w:
+			w.write(learn+'\n')
+		w.close()
 
 	def learn(self):
 		dbase = []
-		with open('tech1.json', 'r') as f:
-			data = json.load(f)
-		for question in data['questions']:
-			dbase.append([question['text'], question['answers'][0]['text']])
+		with open(self.filedb, 'r') as f:
+			data = f.readlines()
+		for question in data:
+			dbase.append([question.split('///')[0], question.split('/// ')[1].split(' # ')])
 		return dbase
 
 	def say(self):
-		print
 		words = []
 		dbase = self.learn()
 		put = uni(raw_input('> ').decode('utf-8')).upper()
 		put = re.sub('[.,?!]', '', put)
-		if put in self.questioned:
-			print 'Creuza: de novo?'
-			return
+		# if put in self.questioned:
+		# 	print 'Creuza: de novo?'
+		# 	return
 		self.questioned.append(put)
 		if len(put.split()) < 2:
+			if put.find('#ADD') != -1:
+				self.add()
+				print
+			if put.find('KKK') != -1 or put.find('HAHA') != -1:
+				print 'Creuza: ha ha ha'
 			if put.find('OI') != -1 or put.find('OLA') != -1:
 				print 'Creuza: Oi, tudo bem?'
+			if put.find('OBRIGAD') != -1:
+				print 'Creuza: por nada :)'
 			if put.find('PRAZER') != -1:
 				print 'Creuza: o prazer é todo seu..'
+			if put.find('TCHAU') != -1:
+				print 'Creuza: vaza!!'
 			if put.find('PORQUE') != -1:
 				print 'Creuza: não enche..'
-
 			return
 		for question in dbase:
 			words = []
@@ -53,14 +67,11 @@ class iabot:
 			# text = re.split('\W+', q)
 			text = q.split()
 			for w in text:
-				# if len(w) > 0:
 				words.append(w)
 			confirm = set(put.split()) & set(words)
-			# print put.split()
-			# if len(confirm):
-			# 	print confirm
 			if len(confirm) == len(put.split()):
-				print 'Creuza:', choice(answer)
+				time.sleep(1)
+				print 'Creuza:', choice(answer).rstrip()
 				return
 
 bot = iabot()
